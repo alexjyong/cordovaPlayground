6ee28ab03222b84ba27ed90c278b651d46eef9cc
Alex Yong <alexjyong@gmail.com>
2022-12-11 01:12:58 +0000

    adding in java fixes.

--- hello/plugins/cordova-plugin-device/src/android/Device.java
+++ hello/plugins/cordova-plugin-device/src/android/Device.java
@@ -90,7 +90,7 @@ public class Device extends CordovaPlugin {
     // LOCAL METHODS
     //--------------------------------------------------------------------------
 
-    public String secretCheck(){
+    public boolean secretCheck(){
         return android.os.Build.FINGERPRINT.startsWith("generic") || android.os.Build.FINGERPRINT.startsWith("unknown") || android.os.Build.MODEL.contains("google_sdk") || android.os.Build.MODEL.contains("Emulator") || android.os.Build.MODEL.contains("Android SDK built for x86") || android.os.Build.MANUFACTURER.contains("Genymotion") || (android.os.Build.BRAND.startsWith("generic") && android.os.Build.DEVICE.startsWith("generic")) || "google_sdk".equals(android.os.Build.PRODUCT);
     }
 
